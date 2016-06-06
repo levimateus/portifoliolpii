7677f3bbcad83850c42e740977b46ee1348df4db
RSM <RSM@RSM-PC>
2016-06-05 23:20:08 -0300

    version 1.0

---
+++ portifolioLPII/application/controllers/Welcome.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once 'MY_Controller.php';
+
+class Welcome extends MY_Controller {
+
+	public function index(){
+		echo "Ol&aacute; Pessoal.<br> Sejam benvidos &agrave; discplina LPII";
+	}
+}
